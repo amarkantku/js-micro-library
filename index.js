b341ca48417aa463ffe2d31aac59a76743755b2a
Amarkant Kumar <amar.du2013@gmail.com>
2021-08-14 16:02:25 +0530

    range rand array commit

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ var myPlugin = (function () {
 		console.log('on');
 	};
 
+	// Use a single function, but make sure to add comments where
+	// you would otherwise have called a helper function.
+	methods.randArrayInRange = (len, a, b) => {
+		const array = new Array(len);
+		for (let i = 0; i < len; ++i) {
+			// Generate random number at least 0, less than 1
+			const randfloat = Math.random();
+			// Move randfloat into [a, b) range
+			array[i] = a + Math.floor(randfloat * (b - a));
+		}
+		return array;
+	}
+
 	// Expose the public methods
 	return methods;
 })();
